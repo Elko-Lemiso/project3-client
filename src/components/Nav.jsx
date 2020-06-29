5d6fb5cef6c68cb231ed9dda7d9e5bd673ee5cfd
Elko Lemiso <elkovandongen@gmail.com>
2020-06-29 16:53:12 +0200

    Nav bar styling

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,24 @@
 import React, { Component } from 'react';
+import plus from '../images/plus.svg'
+import messages from '../images/messages.svg'
+import profile from '../images/profile.svg'
+import list from '../images/list.svg'
+import users from '../images/users.svg'
+// class Nav extends Component {
+//     render() {
+//         return (
+//             <nav>
+//                 <h2>cleanR</h2>
+//                 <ul>
+//                     <li>Sign Up</li>
+//                     <li>Log In</li>
+//                 </ul>
+//             </nav>
+//         );
+//     }
+// }
+
+// export default Nav
 
 class Nav extends Component {
     render() {
@@ -6,8 +26,13 @@ class Nav extends Component {
             <nav>
                 <h2>cleanR</h2>
                 <ul>
-                    <li>Sign Up</li>
-                    <li>Log In</li>
+                    <li><img src={users} alt=""/></li>
+                    <li><img src={list} alt=""/></li>
+                    <div>
+                        <img src={plus} alt=""/>
+                    </div>
+                    <li><img src={messages} alt=""/></li>
+                    <li><img src={profile} alt=""/></li>
                 </ul>
             </nav>
         );
