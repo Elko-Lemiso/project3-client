c3eaada8579eb010c7243232edbfc61bfe30b2a2
Nardo Kraaijeveld <nardo.kra@gmail.com>
2020-07-02 15:49:37 +0200

    html and responsive CSS for jobs page

--- src/pages/userActions/JobsFeed.jsx
+++ src/pages/userActions/JobsFeed.jsx
@@ -13,20 +13,78 @@ class JobsFeed extends Component {
   render() {
     return (
       <div className="big-container jobs-feed">
-        <div className="jobs-column">
-          <div className="jobs-row">
+        <h1>JOBS FEED</h1>
+        <div className="jobs-container">
+          <div className="jobs-card">
             <div className="profile-image-box">
               <img src="https://res.cloudinary.com/dconurgxl/image/upload/v1593453874/avatar-icon-vector_zhcqk3.jpg" alt=""/>
             </div>
-            <div className="profile-details-box">
-              <h3>Name</h3>
-              <div className="avg-rating"></div>
-              <h4>Rating: 5 star</h4>
-              <p>Bio: I am a passionate cleaner, and I love automation. I will bring my robot cleaner too. To double the productivity.</p>
-              <span>25 jobs done.</span>
+            <div className="job-title">
+              <h3 className="job-tagline">Pool cleaning</h3>
+              <div className="hl"></div>
+              <p className="job-price"><strong>Job price:</strong><pre> € </pre>17<span>,-</span><pre> </pre>an hour.</p>
             </div>
+            <p className="job-description">Bio: I am a passionate cleaner, and I love automation. I will bring my robot cleaner too. To double the productivity.</p>
+            <div className="job-footer">
+              <span className="job-owner">Posted by: Elko</span>
+              <span className="job-applications">Running applications: 25</span>
+            </div>
+            <button className="title-blue heartbeat">View job</button>
+          </div>
+
+          <div className="jobs-card">
+            <div className="profile-image-box">
+              <img src="https://res.cloudinary.com/dconurgxl/image/upload/v1593453874/avatar-icon-vector_zhcqk3.jpg" alt=""/>
+            </div>
+            <div className="job-title">
+              <h3 className="job-tagline">Pool cleaning</h3>
+              <div className="hl"></div>
+              <p className="job-price"><strong>Job price:</strong><pre> € </pre>17<span>,-</span><pre> </pre>an hour.</p>
+            </div>
+            <p className="job-description">Bio: I am a passionate cleaner, and I love automation. I will bring my robot cleaner too. To double the productivity.</p>
+            <div className="job-footer">
+              <span className="job-owner">Posted by: Elko</span>
+              <span className="job-applications">Running applications: 25</span>
+            </div>
+            <button className="title-blue heartbeat">View job</button>
+          </div>
+
+          <div className="jobs-card">
+            <div className="profile-image-box">
+              <img src="https://res.cloudinary.com/dconurgxl/image/upload/v1593453874/avatar-icon-vector_zhcqk3.jpg" alt=""/>
+            </div>
+            <div className="job-title">
+              <h3 className="job-tagline">Pool cleaning</h3>
+              <div className="hl"></div>
+              <p className="job-price"><strong>Job price:</strong><pre> € </pre>17<span>,-</span><pre> </pre>an hour.</p>
+            </div>
+            <p className="job-description">Bio: I am a passionate cleaner, and I love automation. I will bring my robot cleaner too. To double the productivity.</p>
+            <div className="job-footer">
+              <span className="job-owner">Posted by: Elko</span>
+              <span className="job-applications">Running applications: 25</span>
+            </div>
+            <button className="title-blue heartbeat">View job</button>
+          </div>
+
+          <div className="jobs-card">
+            <div className="profile-image-box">
+              <img src="https://res.cloudinary.com/dconurgxl/image/upload/v1593453874/avatar-icon-vector_zhcqk3.jpg" alt=""/>
+            </div>
+            <div className="job-title">
+              <h3 className="job-tagline">Pool cleaning</h3>
+              <div className="hl"></div>
+              <p className="job-price"><strong>Job price:</strong><pre> € </pre>17<span>,-</span><pre> </pre>an hour.</p>
+            </div>
+            <p className="job-description">Bio: I am a passionate cleaner, and I love automation. I will bring my robot cleaner too. To double the productivity.</p>
+            <div className="job-footer">
+              <span className="job-owner">Posted by: Elko</span>
+              <span className="job-applications">Running applications: 25</span>
+            </div>
+            <button className="title-blue heartbeat">View job</button>
           </div>
         </div>
+
+        
       </div>
     )
   }
