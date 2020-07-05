a350b96e374b44750ae7aa95715c5afb94cc1f77
Elko Lemiso <elkovandongen@gmail.com>
2020-07-05 12:08:58 +0200

    Pull ready

--- src/pages/userActions/JobsFeed.jsx
+++ src/pages/userActions/JobsFeed.jsx
@@ -29,26 +29,31 @@ class JobsFeed extends Component {
           this.state.jobs.map((oneJob, index) =>{
             debugger
             return(
-              <div>
-                    <div className="jobs-card">
-                <div className="profile-image-box">
-                  <img src="https://res.cloudinary.com/dconurgxl/image/upload/v1593453874/avatar-icon-vector_zhcqk3.jpg" alt=""/>
-                </div>
-                <div className="job-title">
-                  <h3 className="job-tagline">Pool cleaning</h3>
-                  <div className="hl"></div>
-                  <p className="job-price"><strong>Job price:</strong><pre> € </pre>17<span>,-</span><pre> </pre>an hour.</p>
-                </div>
-                <p className="job-description">I am a very busy sales man and I am willing to pay good for a loyal cleaner. Hook me up!</p>
-                <div className="job-footer">
-                  <span className="job-owner">Posted by: Elko</span>
-                  <span className="job-applications">Running applications: 25</span>
-                </div>
-                <button className="title-blue heartbeat"><Link to="/jobsdetailpage">View job</Link></button>
-              </div>
-            </div>
-            )
+              <>
 
+                <div className="jobs-card">
+                  <div className="profile-image-box">
+                    <img src={"https://res.cloudinary.com/dconurgxl/image/upload/v1593453874/avatar-icon-vector_zhcqk3.jpg"} alt=""/>
+                  </div>
+
+                  <div className="job-title">
+                    <h3 className="job-tagline">{oneJob.title}</h3>
+                    <div className="hl"></div>
+                    <p className="job-price"><strong>Job price:</strong><pre> € </pre>{oneJob.rate}<span>,-</span><pre> </pre>an hour.</p>
+                  </div>
+
+                    <p className="job-description">{oneJob.description}</p>
+
+                  <div className="job-footer">
+                    <span className="job-owner">Posted by: Elko</span>
+                    <span className="job-applications">Running applications: 25</span>
+                  </div>
+
+                  <button className="title-blue heartbeat"><Link to="/jobsdetailpage">View job</Link></button>
+                </div>
+
+            </>
+            )
           })
         }
         </div>
