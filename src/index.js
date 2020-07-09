e1c621e1ea9a27b1e66afd7f4eba2b482d2bb1d2
Nardo Kraaijeveld <nardo.kra@gmail.com>
2020-07-09 22:26:36 +0200

    test fix for refresh issue

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Route } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render((
     <Router>
-        <App />
+      <Route path="/" component={App}></Route>
+        {/* <App /> */}
     </Router>
 ), document.getElementById('root'));
 
